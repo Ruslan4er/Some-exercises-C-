ed86e603e2d0a13701a116d3500bc61feb0233ff
Ruslan4er <retra0725@gmail.com>
2018-01-18 00:42:55 +0300

    Little work with system collections

---
+++ 19-System Collections/19-System Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_System_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shop = new Shop();
+            shop.Add("Продукты", "Маруся");
+
+            Console.WriteLine("Продукты покупали:");
+            foreach (var value in shop.FindAllPeopels("Продукты"))
+            {
+                Console.WriteLine(value);
+            }
+            
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
---
+++ 19-System Collections/19-System Collections/Shop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_System_Collections
+{
+    class Shop
+    {
+        private NameValueCollection shop;
+
+
+        public Shop()
+        {
+            shop = new NameValueCollection
+            {
+                {"Продукты", "Алеся"},
+                {"Продукты", "Макс"},
+                {"Продукты", "Антон"},
+                {"Перефирия", "Алеся"},
+                {"Электроника", "Макс"},
+                {"Косметика", "Антон"}
+            };
+        }
+
+        public void Add(string key, string value)
+        {
+            shop.Add(key, value);
+        }
+
+
+        public string[] FindAllPeopels(string key)
+        {
+            return shop.GetValues(key);
+        }
+
+       
+    }
+}
