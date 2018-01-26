8e36caf49f2f8940341eb92b406feb463b700285
Ruslan4er <retra0725@gmail.com>
2018-01-26 08:50:40 +0300

    Add work with critical section

---
+++ 28-Threads-1/28-Threads-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _28_Threads_1
+{
+    class MyClass
+    {
+        private static int counter;
+        private object block = new object();
+
+        public void SomeMethod()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Interlocked.Increment(ref counter);
+                    counter++;
+                    Console.WriteLine("Счетчик - {0}, поток № {1}",
+                        counter, Thread.CurrentThread.GetHashCode());
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            ThreadStart thread = instance.SomeMethod;
+            instance.SomeMethod();
+
+            for (var i = 0; i < 3; i++)
+                new Thread(thread).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 28-Threads-1/Task 2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class FileWorker
+    {
+        FileStream stream1 = new FileStream("FirstDoc.txt", FileMode.Open);
+        FileStream stream2 = new FileStream("SecondDoc.txt", FileMode.Open);
+        TextWriter writer = File.CreateText("ResultDoc.txt");
+
+        private object block = new object();
+
+        public void ReadFile1()
+        {
+            StreamReader reader = new StreamReader(stream1);
+            string res = reader.ReadToEnd();
+            reader.Close();
+            WriteFile(res);
+        }
+
+        public void ReadFile2()
+        {
+            StreamReader reader = new StreamReader(stream2);
+            string res = reader.ReadToEnd();
+            reader.Close();
+            WriteFile(res);
+        }
+
+        private void WriteFile(string content)
+        {
+            lock (block)
+            {
+                writer.WriteLine(content);
+                Console.WriteLine("Поток {0} завершился", Thread.CurrentThread.GetHashCode());
+            }
+        }
+
+        public void CloseThread()
+        {
+            writer.Close();
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            FileWorker instance = new FileWorker();
+
+            Thread thread1 = new Thread(instance.ReadFile1);
+            Thread thread2 = new Thread(instance.ReadFile2);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+            Thread.Sleep(200);
+            instance.CloseThread();
+            Console.ReadKey();
+
+
+        }
+    }
+}
