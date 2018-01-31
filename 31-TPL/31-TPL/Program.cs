aee909378eaca27952f0709da257aacee16b2bb8
Ruslan4er <retra0725@gmail.com>
2018-01-31 22:13:16 +0300

    Add work with Tasks and Parallel libraries

---
+++ 31-TPL/31-TPL/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _31_TPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] myList = new int[100000000];
+            //List<int> myList = new List<int>();
+            Stopwatch timer = new Stopwatch();
+
+            timer.Start();
+            for (int i = 0; i < 100000000; i++)
+            {
+                myList[i] = rnd.Next(1, 500);
+                //myList.Add(rnd.Next(1, 500));
+            }
+            timer.Stop();
+            Console.WriteLine("Массив инициализировался - {0} сек", timer.Elapsed);
+            timer.Reset();
+
+            #region Параллельная выборка
+            GC.Collect();
+            timer.Start();
+            var result = from element in myList.AsParallel()
+                         where element % 2 == 0
+                         select element;
+            timer.Stop();
+            Console.WriteLine("Параллальная выборка - {0} сек, количество- {1}", timer.Elapsed, result.Count());
+            timer.Reset();
+            #endregion
+
+            #region Обычная выборка
+            GC.Collect();
+            timer.Start();
+            var result1 = from e in myList
+                          where e % 2 == 0
+                          select e;
+            timer.Stop();
+            Console.WriteLine("Обычная выборка - {0} сек, количество- {1}", timer.Elapsed, result1.Count());
+            timer.Reset();
+            #endregion
+        }
+    }
+}
+
---
+++ 31-TPL/Task 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Method1()
+        {
+            Thread.CurrentThread.IsBackground = false;
+            Console.WriteLine("Метод 1 начал выполняться");
+            Thread.Sleep(1000);
+            Console.WriteLine("Метод 1 закончил выполняться");
+        }
+
+        static void Method2()
+        {
+            Thread.CurrentThread.IsBackground = false;
+            Console.WriteLine("Метод 2 начал выполняться");
+            Thread.Sleep(2000);
+            Console.WriteLine("Метод 2 закончил выполняться");
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("Основной поток запущен.");
+            
+            Task.Factory.StartNew(() => Parallel.Invoke(Method1, Method2));
+            
+            Console.WriteLine("Основной поток завершен.");
+            Console.ReadKey();
+        }
+    }
+}
