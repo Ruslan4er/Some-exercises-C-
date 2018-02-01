68467de47c62c1adfe71d2a446d43df72b79ef93
Ruslan4er <retra0725@gmail.com>
2018-02-02 01:15:12 +0300

    Add work with AsyncAwait

---
+++ 32-AsyncAwait/32-AsyncAwait/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _32_AsyncAwait
+{
+    class Program
+    {
+        private static int counter;
+        private static object block = new object();
+
+        static async void AsyncMethod()
+        {
+            Console.WriteLine("Первая часть метода в потоке {0}", Thread.CurrentThread.ManagedThreadId);
+            await Task.Run(() =>
+            {
+                lock (block)
+                {
+                    for (int i = 0; i < 10; i++)
+                        Console.WriteLine(++counter);
+                    Console.WriteLine("Вторая часть метода в потоке {0}",
+                        Thread.CurrentThread.ManagedThreadId);
+                }
+            });
+        }
+
+        static void Main()
+        {
+            for (int i = 0; i < 3; i++)
+                AsyncMethod();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 32-AsyncAwait/Task 2/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Forms.Timer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new System.Windows.Forms.Timer {Interval = 4000};
+            timer.Tick += timer_Tick;
+        }
+
+        private async void timer_Tick(object sender, EventArgs e)
+        {
+            textBox.Text += await GetDataAsync();
+            textBox.ScrollToEnd();
+        }
+
+        private Task<string> GetDataAsync()
+        {
+            return Task.Run(() => "data received" + Environment.NewLine);
+        }
+        
+        private async void ConnectDBButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            ConnectDBButton.IsEnabled = false;
+            textBox.Text += await ConnectDBAsync();
+            DisconnectDBButton.IsEnabled = true;
+        }
+
+        private async Task<string> ConnectDBAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return "Connected" + Environment.NewLine;
+            });
+        }
+
+        private async void DisconnectDBButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            DisconnectDBButton.IsEnabled = false;
+            textBox.Text += await DisconnectDBAsync();
+            ConnectDBButton.IsEnabled = true;
+        }
+
+        private async Task<string> DisconnectDBAsync()
+        {
+           return await Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return "Disconnected" + Environment.NewLine;
+            });
+        }
+    }
+}
